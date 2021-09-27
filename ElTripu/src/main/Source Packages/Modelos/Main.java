267bcb960a01d3b9247a2ac69e23edddddfc3d0c
Sergio <sergio>
2021-09-26 19:46:42 -0500

    "Creación carpeta backend Other packages. Creación de packages Modelo y Controlador"

---
+++ ElTripu/src/main/Source Packages/Modelos/Main.java
@@ -0,0 +1,28 @@
+
+import Modelos.Conexion;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Conexion ObjConexion = new Conexion();
+        
+        ObjConexion.conectar();
+        
+    }
+    
+}
