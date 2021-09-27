3b479ff6eecde53dc09867111cac42649d77a513
15db-0005 <15db-0005@LAPTOP-IV5SRV06>
2021-09-26 21:59:34 -0500

    Usuario, TipoiDocumento, Aerolinea (modelo, controlador)

---
+++ ElTripu/src/main/java/Modelos/TipoiDocumento.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Modelos;
+
+/**
+ *
+ * @author 15db-0005
+ */
+public class TipoiDocumento {
+
+//Atributos
+    
+    int id_tipodocumento;
+    String desc_tipodoc;
+    
+    
+    //CONSTRUCTOR
+     public TipoiDocumento(){
+     }
+     
+     //ENCAPSULAR 
+
+    public int getid_tipodocumento() {
+        return id_tipodocumento;
+    }
+
+    public void setid_tipodocumento(int id_tipodocumento) {
+        this.id_tipodocumento = id_tipodocumento;
+    }
+
+    public String getdesc_tipodoc() {
+        return desc_tipodoc;
+    }
+
+    public void setdesc_tipodoc(String desc_tipodoc) {
+        this.desc_tipodoc = desc_tipodoc;
+    }
+
+    // METODOS
+
+    public void crearTipoiDocumento(){
+        
+    }
+    
+    public void consultarTipoiDocumento(){
+        
+    }
+    
+    
+    public void actualizarTipoiDocumento(){
+        
+    }
+    
+    public void eliminarTipoiDocumento(){
+        
+    }
+
+
+
+
+
+
+}
---
+++ ElTripu/src/main/java/Modelos/Usuario.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Modelos;
+
+/**
+ *
+ * @author 15db-0005
+ */
+public class Usuario {
+
+}
+//Atributos
+    
+    int id_usuario;
+    String nombre_usuario;
+    int tipodoc_usuario;
+    String numdoc_usuario;
+    String telefono_usuario;
+    String correo_usuario;
+    String fecnac_usuario;
+    int id_menor;
+    
+    //CONSTRUCTOR
+     public Usuario(){
+     }
+     
+     //ENCAPSULAR 
+
+    public int getid_usuario() {
+        return id_usuario;
+    }
+
+    public void setid_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getnombre_usuario() {
+        return nombre_usuario;
+    }
+
+    public void setnombre_usuario(String nombre_usuario) {
+        this.nombre_usuario = nombre_usuario;
+    }
+
+    public int gettipodoc_usuario() {
+        return tipodoc_usuario;
+    }
+
+    public void settipodoc_usuario(int tipodoc_usuario) {
+        this.tipodoc_usuario = tipodoc_usuario;
+    }
+
+    public String getnumdoc_usuario() {
+        return numdoc_usuario;
+    }
+
+    public void setnumdoc_usuario(String numdoc_usuario) {
+        this.numdoc_usuario = numdoc_usuario;
+    }
+
+    public String gettelefono_usuario() {
+        return telefono_usuario;
+    }
+
+    public void settelefono_usuario(String telefono_usuario) {
+        this.telefono_usuario = telefono_usuario;
+    }
+     
+      public String getcorreo_usuario() {
+        return correo_usuario;
+    }
+
+    public void setcorreo_usuario(String correo_usuario) {
+        this.correo_usuario = correo_usuario;
+    }
+    
+      public String getfecnac_usuario() {
+        return fecnac_usuario;
+    }
+
+    public void setfecnac_usuario(String fecnac_usuario) {
+        this.fecnac_usuario = fecnac_usuario;
+    }
+    
+    public int getid_menor() {
+        return id_menor;
+    }
+
+    public void setid_menor(int id_menor) {
+        this.id_menor = id_menor;
+    }
+      
+
+// METODOS
+
+    public void crearUsuario(){
+        
+    }
+    
+    public void consultarUsuario(){
+        
+    }
+    
+    
+    public void actualizarUsuario(){
+        
+    }
+    
+    public void eliminarUsuario(){
+        
+    }
