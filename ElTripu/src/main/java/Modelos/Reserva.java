4a441db1c3da47ecad782fe0613926a6cfe35f5d
Sergio <sergio>
2021-09-26 21:23:13 -0500

    "Creación carpeta backend Source. Creación de packages Modelo y Controlador"

---
+++ ElTripu/src/main/java/Modelos/Reserva.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Reserva {
+    //Atributos
+    
+    int reserva;
+    int id_usuario;
+    int id_vuelo;
+    String hora_reserva;
+    String fecha_reserva;
+    
+    
+    //CONSTRUCTOR
+     public Reserva(){
+     }
+     
+     //ENCAPSULAR 
+
+    public int getReserva() {
+        return reserva;
+    }
+
+    public void setReserva(int reserva) {
+        this.reserva = reserva;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public int getId_vuelo() {
+        return id_vuelo;
+    }
+
+    public void setId_vuelo(int id_vuelo) {
+        this.id_vuelo = id_vuelo;
+    }
+
+    public String getHora_reserva() {
+        return hora_reserva;
+    }
+
+    public void setHora_reserva(String hora_reserva) {
+        this.hora_reserva = hora_reserva;
+    }
+
+    public String getFecha_reserva() {
+        return fecha_reserva;
+    }
+
+    public void setFecha_reserva(String fecha_reserva) {
+        this.fecha_reserva = fecha_reserva;
+    }
+     
+     
+
+// METODOS
+
+    public void crearReserva(){
+        
+    }
+    
+    public void consultarReserva(){
+        
+    }
+    
+    public void listarReserva(){
+        
+    }
+    
+    public void actualizarReserva(){
+        
+    }
+    
+    public void eliminarReserva(){
+        
+    }
+}
+
+    
+    
+    
+    
+    
+
