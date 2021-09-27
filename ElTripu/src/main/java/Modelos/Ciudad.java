bb7f37c28e163aaf2493a27a47d107e4e1f0b86f
Sergio <sergio>
2021-09-26 22:02:37 -0500

    "Creación clases Vuelo, Reserva y Ciudad, con sus respectivos controladores servlet"

---
+++ ElTripu/src/main/java/Modelos/Ciudad.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author User
+ */
+public class Ciudad {
+    //Atributos
+    int id_ciudad;
+    String desc_ciudad;
+    int id_pais; 
+
+  //CONSTRUCTOR
+ public Ciudad(){
+ }
+   //ENCAPSULAR 
+
+    public int getId_ciudad() {
+        return id_ciudad;
+    }
+
+    public void setId_ciudad(int id_ciudad) {
+        this.id_ciudad = id_ciudad;
+    }
+
+    public String getDesc_ciudad() {
+        return desc_ciudad;
+    }
+
+    public void setDesc_ciudad(String desc_ciudad) {
+        this.desc_ciudad = desc_ciudad;
+    }
+
+    public int getId_pais() {
+        return id_pais;
+    }
+
+    public void setId_pais(int id_pais) {
+        this.id_pais = id_pais;
+    }
+ 
+    public void crearCiudad(){
+        
+    }
+    
+    public void consultarCiudad(){
+        
+    }
+    
+    public void listarCiudad(){
+        
+    }
+    
+    public void actualizarCiudad(){
+        
+    }
+    
+    public void eliminarCiudad(){
+        
+    }
+
+}
---
+++ ElTripu/src/main/java/Modelos/Vuelo.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author User
+ */
+public class Vuelo {
+    //Atributos
+    int id_vuelo;
+    int origen_vuelo;
+    int destino_vuelo;
+    String fechaida_vuelo;
+    String fecharegreso_vuelo;
+    String horasalida_vuelo;
+    String horaregreso_vuelo;
+    int id_categoria;
+    float precio_vuelo;
+    int sillas_vuelo;
+    int id_aerolinea;
+    
+     //CONSTRUCTOR
+    
+    public Vuelo(){
+    }
+    
+    //ENCAPSULAR 
+
+    public int getId_vuelo() {
+        return id_vuelo;
+    }
+
+    public void setId_vuelo(int id_vuelo) {
+        this.id_vuelo = id_vuelo;
+    }
+
+    public int getOrigen_vuelo() {
+        return origen_vuelo;
+    }
+
+    public void setOrigen_vuelo(int origen_vuelo) {
+        this.origen_vuelo = origen_vuelo;
+    }
+
+    public int getDestino_vuelo() {
+        return destino_vuelo;
+    }
+
+    public void setDestino_vuelo(int destino_vuelo) {
+        this.destino_vuelo = destino_vuelo;
+    }
+
+    public String getFechaida_vuelo() {
+        return fechaida_vuelo;
+    }
+
+    public void setFechaida_vuelo(String fechaida_vuelo) {
+        this.fechaida_vuelo = fechaida_vuelo;
+    }
+
+    public String getFecharegreso_vuelo() {
+        return fecharegreso_vuelo;
+    }
+
+    public void setFecharegreso_vuelo(String fecharegreso_vuelo) {
+        this.fecharegreso_vuelo = fecharegreso_vuelo;
+    }
+
+    public String getHorasalida_vuelo() {
+        return horasalida_vuelo;
+    }
+
+    public void setHorasalida_vuelo(String horasalida_vuelo) {
+        this.horasalida_vuelo = horasalida_vuelo;
+    }
+
+    public String getHoraregreso_vuelo() {
+        return horaregreso_vuelo;
+    }
+
+    public void setHoraregreso_vuelo(String horaregreso_vuelo) {
+        this.horaregreso_vuelo = horaregreso_vuelo;
+    }
+
+    public int getId_categoria() {
+        return id_categoria;
+    }
+
+    public void setId_categoria(int id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+
+    public float getPrecio_vuelo() {
+        return precio_vuelo;
+    }
+
+    public void setPrecio_vuelo(float precio_vuelo) {
+        this.precio_vuelo = precio_vuelo;
+    }
+
+    public int getSillas_vuelo() {
+        return sillas_vuelo;
+    }
+
+    public void setSillas_vuelo(int sillas_vuelo) {
+        this.sillas_vuelo = sillas_vuelo;
+    }
+
+    public int getId_aerolinea() {
+        return id_aerolinea;
+    }
+
+    public void setId_aerolinea(int id_aerolinea) {
+        this.id_aerolinea = id_aerolinea;
+    }
+    
+    // METODOS
+
+    public void crearVuelo(){
+        
+    }
+    
+    public void consultarVuelo(){
+        
+    }
+    
+    public void listarVuelo(){
+        
+    }
+    
+    public void actualizarVuelo(){
+        
+    }
+    
+    public void eliminarVuelo(){
+        
+    }
+}
