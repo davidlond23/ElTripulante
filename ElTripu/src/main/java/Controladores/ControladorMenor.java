3898e713d521143ea91bb9a37f5824fe2b1427f8
Sergio <sergio>
2021-10-06 19:26:37 -0500

    Ajustes controladores y Modelo Menor: eliminar y listar

--- ElTripu/src/main/java/Controladores/ControladorMenor.java
+++ ElTripu/src/main/java/Controladores/ControladorMenor.java
@@ -5,8 +5,11 @@
  */
 package Controladores;
 
+import Modelos.Menor;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,6 +23,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "Menor", urlPatterns = {"/Menor"})
 public class ControladorMenor extends HttpServlet {
 
+    Menor objMenor = new Menor(); 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -34,18 +38,81 @@ public class ControladorMenor extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Menor</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Menor at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            String accion = request.getParameter("btnAccion"); 
+            
+            if(accion.equals("Reservar")){
+                             
+                int id_menor = Integer.parseInt(request.getParameter("id_menor"));
+                int id_usuario = Integer.parseInt(request.getParameter("id_usuario"));
+                int tipodoc_menor = Integer.parseInt(request.getParameter("tipodoc_menor"));
+                String numdoc_menor = request.getParameter("numdoc_menor"); 
+                String fecnac_menor = request.getParameter("fecnac_menor"); 
+                
+                objMenor.setId_menor(id_menor);
+                objMenor.setId_usuario(id_usuario);
+                objMenor.setTipodoc_menor(tipodoc_menor);
+                objMenor.setNumdoc_menor(numdoc_menor);
+                objMenor.setFecnac_menor(fecnac_menor);
+                
+                objMenor.crearMenor();
+                
+                String mensaje = " <html> <body>"+
+                                 "<script type='text/javaScript'> "+
+                                 "  alert('Menor registrado correctamente'); "+
+                                 "  window.location.href='index.jsp'; "+
+                                 "</script> </body> </html>"; 
+                
+                out.println(mensaje); 
+            }
+            else if (accion.equals("Actualizar")){
+                
+                int id_menor = Integer.parseInt(request.getParameter("id_menor"));
+                int id_usuario = Integer.parseInt(request.getParameter("id_usuario"));
+                int tipodoc_menor = Integer.parseInt(request.getParameter("tipodoc_menor"));
+                String numdoc_menor = request.getParameter("numdoc_menor"); 
+                String fecnac_menor = request.getParameter("fecnac_menor"); 
+                
+                objMenor.setId_menor(id_menor);
+                objMenor.setId_usuario(id_usuario);
+                objMenor.setTipodoc_menor(tipodoc_menor);
+                objMenor.setNumdoc_menor(numdoc_menor);
+                objMenor.setFecnac_menor(fecnac_menor);
+                
+                objMenor.actualizarMenor();
+                
+                String mensaje = " <html> <body>"+
+                                 "<script type='text/javaScript'> "+
+                                 "  alert('Menor actualizado correctamente'); "+
+                                 "  window.location.href='index.jsp'; "+
+                                 "</script> </body> </html>"; 
+                
+                System.out.println(mensaje); 
+            }
+            else if (accion.equals("Eliminar")){
+                
+                int id_menor = Integer.parseInt(request.getParameter("id_menor"));
+                                
+                objMenor.setId_menor(id_menor);
+                
+                objMenor.eliminarMenor();
+                
+                String mensaje = " <html> <body>"+
+                                 "<script type='text/javaScript'> "+
+                                 "  alert('Menor eliminado correctamente'); "+
+                                 "  window.location.href='index.jsp'; "+
+                                 "</script> </body> </html>"; 
+                
+               System.out.println(mensaje); 
+            }
+            
+            
+            
         }
+        catch(Exception error){
+            System.out.println("Error en el controlador: "+error);
+        }
+   
     }
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -84,5 +151,55 @@ public class ControladorMenor extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
+public ArrayList consultar(int id_menor){
+        
+        try{
+            objMenor.setId_menor(id_menor);
+            ResultSet consulta = objMenor.consultarMenor(); 
+            ArrayList<Menor> listaMenor = new ArrayList<>(); 
+            
+            while(consulta.next()){
+                objMenor = new Menor(); 
+                objMenor.setId_menor(consulta.getInt(1));
+                objMenor.setId_usuario(consulta.getInt(2));
+                objMenor.setTipodoc_menor(consulta.getInt(3));
+                objMenor.setNumdoc_menor(consulta.getString(4));
+                objMenor.setFecnac_menor(consulta.getString(5));
+                listaMenor.add(objMenor); 
+            }
+            return listaMenor; 
+        }
+        catch(Exception error){
+            System.out.println("Error de controlador: "+ error);
+        }  
+        return null; 
+    }
+    
+    public ArrayList listar(){
+        
+        try {
+            ResultSet consulta = objMenor.listarMenor();
+            ArrayList<Menor> listaMenor = new ArrayList<>();
+            
+            while(consulta.next()){
+                objMenor = new Menor();
+                objMenor.setId_menor(consulta.getInt(1));
+                objMenor.setId_usuario(consulta.getInt(2));
+                objMenor.setTipodoc_menor(consulta.getInt(3));
+                objMenor.setNumdoc_menor(consulta.getString(4));
+                objMenor.setFecnac_menor(consulta.getString(5));
+                listaMenor.add(objMenor);
+                               
+            }
+            return listaMenor;
+            
+        } catch (Exception error) {
+            
+            System.out.println("Error de controlador: "+ error);
+        }
+        
+        return null;
+    
 }
+ }
+        
